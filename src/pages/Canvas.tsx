7e3c3db1cf3d8f9df6ca36eb9db5e456e6942665
Valya Tureyskaya <turleo@outlook.com>
2023-11-18 18:34:26 +1000

    ✨ Canvas now is endless

--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -9,7 +9,7 @@ class Drawing {
     this.y = y
   }
 
-  render (ctx: CanvasRenderingContext2D) {}
+  render (ctx: CanvasRenderingContext2D, dx: number, dy: number) {}
 }
 
 class Text extends Drawing {
@@ -24,10 +24,10 @@ class Text extends Drawing {
     this.font = font
   }
 
-  render (ctx: CanvasRenderingContext2D) {
+  render (ctx: CanvasRenderingContext2D, dx: number, dy: number) {
     ctx.font = this.font
     ctx.fillStyle = this.color
-    ctx.fillText(this.text, this.x, this.y)
+    ctx.fillText(this.text, this.x + dx, this.y + dy)
   }
 }
 
@@ -35,6 +35,9 @@ class Text extends Drawing {
 type Props = {}
 type State = {
   drawings: Array<Drawing>
+  mouseClicked: boolean,
+  dx: number
+  dy: number
 }
 
 export default class Canvas extends Component<Props, State> {
@@ -42,7 +45,10 @@ export default class Canvas extends Component<Props, State> {
     drawings: [
       new Text(10, 10, 'Hi!', 'white', '12px serif'),
       new Text(10, 600, 'meow', 'yellow', '500px monospace'),
-    ]
+    ],
+    mouseClicked: false,
+    dx: 0,
+    dy: 0
   }
   canvasRef = createRef<HTMLCanvasElement>()
 
@@ -57,13 +63,33 @@ export default class Canvas extends Component<Props, State> {
     }
     ctx.clearRect(0,0, 1000, 1000);
     this.state.drawings.forEach(drawing => {
-      drawing.render(ctx)
+      drawing.render(ctx, this.state.dx, this.state.dy)
     })
+  }
+
+  moveElements(e: any) {
+    if (!this.state.mouseClicked) {
+      return
+    }
+    this.state.dx += e.movementX
+    this.state.dy += e.movementY
+    this.updateCanvas()
   }
 
   render() {
     return (
-      <canvas ref={this.canvasRef} width='1000' height='1000' onClick={() => {this.updateCanvas()}}></canvas>
+      <canvas ref={this.canvasRef} width='1000' height='1000' 
+          //onClick={() => {this.updateCanvas()}} 
+          onPointerMove={(e) => { this.moveElements(e) }}
+          onMouseDown={() => {
+            this.state.mouseClicked = true
+            this.setState(this.state)
+          }}
+          onMouseUp={() => {
+            this.state.mouseClicked = false
+            this.setState(this.state)
+          }}
+        ></canvas>
     )
   }
 }
