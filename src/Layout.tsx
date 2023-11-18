b952e6ee44c9e00e5f5a55dff25516b45fa197d3
Valya Tureyskaya <turleo@outlook.com>
2023-11-18 23:38:51 +1000

    💄 Layout design added

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,25 @@
-import { useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+
+import farpost from './assets/farpost.svg'
+import incoming from './assets/incoming.svg'
+import people from './assets/people.svg'
+import whiteboard from './assets/whiteboard.svg'
 
 
 export default function Layout() {
   return (
-    <div>
-      { window.localStorage.getItem('loginState') ? 
-        <p>Welcome</p> 
-      :
-        <p>Not logged in <Link to='login'>Please login</Link></p>
-      }
-      <Outlet />
+    <div id="app">
+      <header>
+        <div id='icons'>
+          <Link to='/'><img src={farpost} /></Link>
+          <Link to='/people/'><img src={people} /></Link>
+          <Link to='/incoming/'><img src={incoming} /></Link>
+          <Link to='/canvas/'><img src={whiteboard} /></Link>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
